9c666e97775b3580ab343e5efad9ad869c97bd6d
sxwgf <sxwgf.com@gmail.com>
2014-08-31 06:54:10 +0800

    55
    44

--- gg_bd_ad_720x90.js
+++ gg_bd_ad_720x90.js
@@ -27,11 +27,11 @@ document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static
 
 document.writeln("<div style=\"width:728px;margin:10px auto;\">")
 document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
-document.writeln('<!-- 评道demo横幅 -->');
+document.writeln('<!-- github demo -->');
 document.writeln('<ins class="adsbygoogle"');
 document.writeln('     style="display:inline-block;width:728px;height:90px"');
 document.writeln('     data-ad-client="ca-pub-3171310320403916"');
-document.writeln('     data-ad-slot="5686750586"></ins>');
+document.writeln('     data-ad-slot="4251965788"></ins>');
 document.writeln('<script>');
 document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
 document.writeln('</script>');
